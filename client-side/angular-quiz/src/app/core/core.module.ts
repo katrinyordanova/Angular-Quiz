f8b89d28519e02af97a26ffd23103e699b636972
Katrin Yordanova <katrinyordanova@abv.bg>
2020-02-11 14:24:36 +0200

    added and styled footer

--- client-side/angular-quiz/src/app/core/core.module.ts
+++ client-side/angular-quiz/src/app/core/core.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [NavigationComponent],
+  declarations: [NavigationComponent, FooterComponent],
   imports: [
     CommonModule
   ], 
   exports: [
-    NavigationComponent
+    NavigationComponent,
+    FooterComponent
   ]
 })
 export class CoreModule { }
